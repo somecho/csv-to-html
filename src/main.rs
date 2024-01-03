7f3016bdb3e95503d33d4f74c7a3a7ed12a1266b
Some Cho <itssomicho@gmail.com>
2024-01-03 16:25:07 +0100

    add output flag

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use build_html::{Html, Table};
 use clap::Parser;
+use std::fs::{self, File};
 
 /// Convert CSV table to HTML table
 #[derive(Parser, Debug)]
-#[command(version)]
+#[command(author, version)]
 struct Args {
     /// path to input file
     #[arg(short, long)]
@@ -11,9 +12,12 @@ struct Args {
     /// the CSV file has no header
     #[arg(short, long)]
     no_header: bool,
+    /// path of output file. If not specified, will print to stdout
+    #[arg(short, long)]
+    output: Option<String>,
 }
 
-fn csv_to_html(csv_reader: &mut csv::Reader<std::fs::File>, has_header: &bool) -> String {
+fn csv_to_html(csv_reader: &mut csv::Reader<File>, has_header: &bool) -> String {
     let mut table = Table::new();
     match csv_reader.headers() {
         Ok(record) => {
@@ -24,15 +28,13 @@ fn csv_to_html(csv_reader: &mut csv::Reader<std::fs::File>, has_header: &bool) -
                 table.add_body_row(header);
             }
         }
-        Err(_) => {}
+        Err(_) => (),
     }
     let records = csv_reader.records();
     for r in records {
         match r {
-            Ok(record) => {
-                table.add_body_row(record.iter().collect::<Vec<&str>>());
-            }
-            Err(_) => {}
+            Ok(record) => table.add_body_row(record.iter().collect::<Vec<&str>>()),
+            Err(_) => (),
         }
     }
     table.to_html_string()
@@ -45,7 +47,10 @@ fn main() {
     match reader {
         Ok(mut r) => {
             let html: String = csv_to_html(&mut r, &!args.no_header);
-            println!("{}", html);
+            match args.output {
+                Some(path) => fs::write(path, html).expect("Unable to write file"),
+                None => println!("{}", html),
+            }
         }
         Err(err) => {
             eprintln!("{}", err);
